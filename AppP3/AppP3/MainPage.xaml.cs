c8b8e12da470062fa50e16cecf5fd143ea055394
DESKTOP-CSE0K64\albin <asandi01@gmail.com>
2018-04-22 23:43:57 -0600

    Show data form WS

--- AppP3/AppP3/MainPage.xaml.cs
+++ AppP3/AppP3/MainPage.xaml.cs
@@ -1,21 +1,65 @@
-﻿using System;
+﻿using AppP3.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace AppP3 {
     public partial class MainPage : ContentPage {
+        //IEnumerable<PaymentRecordModel> listPayment;// = new IEnumerable<PaymentRecordModel>();
+        List<string> listPayment = new List<string>();
         public MainPage() {
             InitializeComponent();
+            //lst.ItemsSource = new List<string>() { "item 1", "item 2", "Item 3" };  
+
             var wsPayment = new ServiceReference2.PaymentRecordSoapClient();
             wsPayment.GetDetailsAsync();
             wsPayment.GetDetailsCompleted += wsPaymentCompleted;
         }
 
-        private void wsPaymentCompleted(object sender, ServiceReference2.GetDetailsCompletedEventArgs e) {
-            var results = e.Result.GetEnumerator();     
+
+        protected async override void OnAppearing() {
+            base.OnAppearing();   
+        }
+
+
+
+        private async void wsPaymentCompleted(object sender, ServiceReference2.GetDetailsCompletedEventArgs e) {
+            var results = e.Result;
+            //listPayment = results.Cast<PaymentRecordModel>();
+
+            foreach (var item in results) { 
+                /*listPayment.Add(new PaymentRecordModel() => {
+                      item.amount,
+                      item.id,
+                      item.paymentDate,
+                      item.providerId,
+                      item.recurrence
+                }); */
+                listPayment.Add(item.id +" => " +item.detail + " " + item.amount);
+            }
+
+            Device.BeginInvokeOnMainThread(() => {
+                lst.ItemsSource = listPayment;    
+            });
+ 
+
+        }
+
+
+
+        async void OnAddItemClicked(object sender, EventArgs e) {    
+        }
+
+
+        async void OnItemSelected(object sender, SelectedItemChangedEventArgs e) {
+            string itemSelected = e.SelectedItem.ToString();
+            string[] id = itemSelected.Split(' ');
+
+            int idSelected = Int32.Parse(id[0]);
+            await Navigation.PushModalAsync(new EditPayment(idSelected));
+            //await Navigation.PushAsync(EditPayment);
         }
 
 
